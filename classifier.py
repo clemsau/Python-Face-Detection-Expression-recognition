02c1078fe8f07b76490cf708009a11b36c14baf1
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-04-06 17:15:35 +0200

    Add requirements and test recognition

---
+++ classifier.py
@@ -0,0 +1,16 @@
+from deepface import DeepFace
+
+import cv2
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+img_path = "img/surprise.jpg"
+
+image = cv2.imread(img_path)
+
+plt.imshow(image)
+
+result = DeepFace.analyze(img_path)
+
+print(result)
