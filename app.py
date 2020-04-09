f9dd3e6581ff0eed1750fee055ca76ca024413bc
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-04-09 19:38:49 +0200

    Implement Flask to classify images

---
+++ app.py
@@ -0,0 +1,30 @@
+# Flask imports
+import os
+
+from flask import Flask,  request, jsonify
+from config import SECRET_KEY, UPLOAD_PATH
+from werkzeug.utils import secure_filename
+
+# Classifier imports
+from deepface import DeepFace
+import cv2
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+app.secret_key = SECRET_KEY
+app.config['UPLOAD_FOLDER'] = UPLOAD_PATH
+
+
+@app.route('/', methods=['POST'])
+def index():
+    json = {}
+    if 'image' in request.files:
+        image = request.files['image']
+        if image.filename != '':
+            image_name = secure_filename(image.filename)
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_name)
+            image.save(image_path)
+            json = DeepFace.analyze(image_path)
+    return jsonify(json)
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+APP_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# Uploads
+UPLOAD_PATH = os.path.join(APP_PATH, 'uploads/')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+SECRET_KEY = 'm6@n^tk&t)t$#*$!@vyme&g4@o^xdsx0tvi)q1x5e3^x@f5nqk'
