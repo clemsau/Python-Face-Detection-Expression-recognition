44e1c90fe90a581e7eba654866d810164d5a328f
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-04-10 14:52:33 +0200

    Add test index

--- app.py
+++ app.py
@@ -16,15 +16,17 @@ app = Flask(__name__)
 app.secret_key = SECRET_KEY
 app.config['UPLOAD_FOLDER'] = UPLOAD_PATH
 
-
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    json = {}
-    if 'image' in request.files:
-        image = request.files['image']
-        if image.filename != '':
-            image_name = secure_filename(image.filename)
-            image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_name)
-            image.save(image_path)
-            json = DeepFace.analyze(image_path)
-    return jsonify(json)
+    if request.method == 'GET':
+        return UPLOAD_PATH + "index.html"
+    if request.method == 'POST':
+        json = {}
+        if 'image' in request.files:
+            image = request.files['image']
+            if image.filename != '':
+                image_name = secure_filename(image.filename)
+                image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_name)
+                image.save(image_path)
+                json = DeepFace.analyze(image_path)
+        return jsonify(json)
