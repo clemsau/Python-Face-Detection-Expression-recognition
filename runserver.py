f20ba7cd3159f53bb0dd561e4cc73286ff614531
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-04-10 14:59:46 +0200

    Change local host runner

--- runserver.py
+++ runserver.py
@@ -5,7 +5,7 @@ from app import app
 
 def runserver():
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='localhost', port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
